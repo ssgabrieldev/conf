549f2c175bfa4677a7d2fde5cf323891cc465576
ssgabrieldev <ssgabrieldev@gmail.com>
2024-02-17 11:36:26 -0300

    nvim: tabuffline lazy load off

--- nvim/custom/chadrc.lua
+++ nvim/custom/chadrc.lua
@@ -1,17 +1,20 @@
 local M = {}
 
-local highlights = require "custom.highlights"
+local highlights = require("custom.highlights")
 
 M.ui = {
-  theme = "onedark",
-  theme_toggle = { "onedark", "one_light" },
+	theme = "onedark",
+	theme_toggle = { "onedark", "one_light" },
 
-  hl_override = highlights.override,
-  hl_add = highlights.add,
+	hl_override = highlights.override,
+	hl_add = highlights.add,
+	tabufline = {
+		lazyload = false
+	},
 }
 
 M.plugins = "custom.plugins"
 
-M.mappings = require "custom.mappings"
+M.mappings = require("custom.mappings")
 
 return M
